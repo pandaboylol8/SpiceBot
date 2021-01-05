94215018bfd2141e5bc3ff01da3a738104c8ab9b
regulad <parkeredwardwahle2017@gmail.com>
2021-01-05 18:00:09 +0000

    G U M M I

--- main.py
+++ main.py
@@ -2,30 +2,35 @@ import discord
 from discord.ext import commands
 import os, requests, json
 
+def random_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+  return(quote)
+
 bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
   print('Logged in as ' + str(bot.user.name) + '#' + str(bot.user.discriminator) + '(' + str(bot.user.id) + ')')
-    
-@bot.command(name='ez',description='gg no re',category="funni")
-async def _bot(ctx):
-  await ctx.send("https://tenor.com/view/ez-yann-gauthier-gif-18979624")
 
-@bot.command(name='quote',description='Get Inspired!',category="funni",aliases=['inspire'])
+@bot.command(name='quote',description='Get Inspired!',aliases=['inspire'])
 async def quote(ctx):
-  def random():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + " - " + json_data[0]['a']
-    return(quote)
-  quote = random()
+  quote = random_quote()
   embed = discord.Embed(
     title=quote
   )
   await ctx.send(embed=embed)
 
-@bot.command(name='derg',description='Nice doggy!',category="funni",aliases=['dog'])
+@bot.command(name='ez',description='gg no re')
+async def _bot(ctx):
+  await ctx.send("https://tenor.com/view/ez-yann-gauthier-gif-18979624")
+  
+@bot.command(name='gummi',description='🐻🐻🐻🐻 🧟 👉😗👈 🌪',aliases=['gummy'])
+async def _bot(ctx):
+  await ctx.send("https://vm.tiktok.com/ZMJWJQCSH/")
+
+@bot.command(name='derg',description='Nice doggy!',aliases=['dog'])
 async def derg(ctx):
   embed = discord.Embed(
     title="░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░\n░█░░░░░░░░▀▄░░░░░░▄░\n█░░▀░░▀░░░░░▀▄▄░░█░█\n█░▄░█▀░▄░░░░░░░▀▀░░█\n█░░▀▀▀▀░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░░░█\n█░░░░░░░░░░░░░░░░░░█\n░█░░▄▄░░▄▄▄▄░░▄▄░░█░\n░█░▄▀█░▄▀░░█░▄▀█░▄▀░\n░░▀░░░▀░░░░░▀░░░▀░░░"
